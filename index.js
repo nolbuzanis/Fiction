2741d5f8f29bc4f2a3a6ed38471281f492e08a01
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-28 18:52:59 -0400

    Finished up with authentication on Passport side.

--- index.js
+++ index.js
@@ -10,18 +10,39 @@ const PORT = process.env.PORT || 5000;
 // Passport
 
 passport.use(
-  new GoogleStrategy({
-    clientID: googleClientID,
-    clientSecret: googleClientSecret,
-    callbackURL: 'http://localhost:5000/auth/google/callback	'
-  }),
-  accessToken => {
-    console.log(accessToken);
-  }
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: 'http://localhost:5000/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token: ', accessToken);
+      console.log('refresh token: ', refreshToken);
+      console.log('profile: ', profile);
+      console.log('done: ', done);
+    }
+  )
 );
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  () => {
+    console.log('Sucessfully logged in!');
+    //res.redirect('/');
+  }
+);
 
 app.listen(PORT, () => console.log('Now listening on Port: ' + PORT));
