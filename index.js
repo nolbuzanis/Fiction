b5ea4c90c390759ed2b598ca5fe2ee8783657512
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-29 13:30:23 -0400

    Wired up mongoose to mongoDB database, hosted remotely via Atlas

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Mongoose
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 // Passport
 require('./routes/authRoutes')(app);
