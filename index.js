65733f6368d2616e1e40a603eae4215eac61d64a
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-29 13:58:14 -0400

    Now user googleID is saved into users collection of database

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-require('./services/passport');
+require('./models/user'); // Load in user schema and users model
+require('./services/passport'); // Load passport config
 
 const app = express();
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const googleClientID = require('../config/keys').googleClientID;
 const googleClientSecret = require('../config/keys').googleClientSecret;
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -11,10 +14,7 @@ passport.use(
       callbackURL: 'http://localhost:5000/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token: ', accessToken);
-      console.log('refresh token: ', refreshToken);
-      console.log('profile: ', profile);
-      console.log('done: ', done);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
